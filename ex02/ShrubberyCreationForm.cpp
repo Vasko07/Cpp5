b247b0e9998ca1f401886b857f4e4615887d8fb1
Vasko <gorkapinillaeij@gmail.com>
2024-11-27 21:47:46 +0100

    ex02 Completed

--- ex02/ShrubberyCreationForm.cpp
+++ ex02/ShrubberyCreationForm.cpp
@@ -6,11 +6,12 @@
 /*   By: gpinilla <gpinilla@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/25 23:23:13 by gpinilla          #+#    #+#             */
-/*   Updated: 2024/11/25 23:33:05 by gpinilla         ###   ########.fr       */
+/*   Updated: 2024/11/27 21:45:48 by gpinilla         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ShrubberyCreationForm.hpp"
+#include <fstream>
 
 ShrubberyCreationForm::ShrubberyCreationForm(const std::string& target)
     : AForm("ShrubberyCreationForm", 145, 137, target) {}
@@ -26,17 +27,26 @@ ShrubberyCreationForm& ShrubberyCreationForm::operator=(const ShrubberyCreationF
 ShrubberyCreationForm::~ShrubberyCreationForm() {}
 
 void ShrubberyCreationForm::performAction() const {
-    std::ofstream outFile(getTarget() + "_shrubbery");
-    if (outFile.is_open()) {
-        outFile << "       _-_       \n";
-        outFile << "    /~~   ~~\\    \n";
-        outFile << " /~~         ~~\\ \n";
-        outFile << "{               }\n";
-        outFile << " \\  _-     -_  / \n";
-        outFile << "   ~  \\\\ //  ~   \n";
-        outFile << "_- -   | | _- _  \n";
-        outFile << "  _ -  | |   -_  \n";
-        outFile << "      // \\\\      \n";
-        outFile.close();
+    std::ofstream outFile((getTarget() + "_shrubbery").c_str()); // Convertir a const char*
+    if (!outFile) {
+        std::cerr << "Error: Could not open file for writing." << std::endl;
+        return;
     }
+
+    outFile <<
+        "         &&& &&  & &&\n"
+        "      && &\\/&\\|& ()|/ @, &&\n"
+        "      &\\/(/&/&||/& /_/)_&/_&\n"
+        "   &() &\\/&|()|/&\\/ '%\" & ()\n"
+        "  &_\\_&&_\\ |& |&&/&__%_/_& &&\n"
+        "&&   && & &| &| /& & % ()& /&&\n"
+        "   ()&_---()&\\&\\|&&-&&--%---()~\n"
+        "            &&     \\|||\n"
+        "                      |||\n"
+        "                      |||\n"
+        "                      |||\n"
+        "                , -=-~  .-^- _\n";
+
+    outFile.close();
+    std::cout << "Shrubbery has been created successfully in " << getTarget() << "_shrubbery" << std::endl;
 }
